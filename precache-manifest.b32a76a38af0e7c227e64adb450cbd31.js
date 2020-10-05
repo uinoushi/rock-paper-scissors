10b0c35f34062ea969bbc57be70f890dbc660b52
Noushad Kottilingal <noushadkottilingal@ind-mac-0003.local>
2020-10-05 11:10:15 +0530

    Updates

---
+++ precache-manifest.b32a76a38af0e7c227e64adb450cbd31.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "657c6e50ed0af78f73d57f59bc6dac7e",
+    "url": "/rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "8c4e88c4b8aae099f851",
+    "url": "/rock-paper-scissors/static/css/2.329fba89.chunk.css"
+  },
+  {
+    "revision": "19fccc7e7b451cc36f9d",
+    "url": "/rock-paper-scissors/static/css/main.12186a76.chunk.css"
+  },
+  {
+    "revision": "8c4e88c4b8aae099f851",
+    "url": "/rock-paper-scissors/static/js/2.1495a306.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rock-paper-scissors/static/js/2.1495a306.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19fccc7e7b451cc36f9d",
+    "url": "/rock-paper-scissors/static/js/main.00f61619.chunk.js"
+  },
+  {
+    "revision": "54d8b3ee6fc233b06722",
+    "url": "/rock-paper-scissors/static/js/runtime-main.5af9a2bb.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/rock-paper-scissors/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/rock-paper-scissors/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/rock-paper-scissors/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/rock-paper-scissors/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/rock-paper-scissors/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
